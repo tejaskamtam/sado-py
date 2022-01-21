82f83a5c1a1b45b5a3f866fdcc9c1e41b39bde82
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-01-21 02:20:52 -0600

    library testing

--- bot.py
+++ bot.py
@@ -1,9 +1,22 @@
 import discord
-import config.py
+import config
 client = discord.Client()
 
 @client.event
 async def on_ready():
     print('sado is ready to make dosas!')
 
-client.run(token)
+@client.event
+async def on_message(msg):
+    if msg.author == client.user:
+        return
+    if msg.content == "-sado":
+        for role in msg.author.roles:
+            if role.name == 'chad(s)':
+                await msg.channel.send("sigma > chad")
+                return
+        await msg.channel.send("sado makin dosas")
+        return
+    
+
+client.run(config.token)
