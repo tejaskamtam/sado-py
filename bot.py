c361588010a4fcd36ade87c233366496ef6c6f68
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-01-21 15:50:03 -0600

    msging updates

--- bot.py
+++ bot.py
@@ -1,22 +1,36 @@
+#   libraries
 import discord
 import config
-client = discord.Client()
+import random
 
-@client.event
+#   variables
+bot = discord.Client()
+ancmt = bot.get_channel('759670370558541845')
+
+@bot.event
 async def on_ready():
-    print('sado is ready to make dosas!')
+    print('sadopy is ready')
 
-@client.event
+@bot.event
 async def on_message(msg):
-    if msg.author == client.user:
+    if msg.author == bot.user:
         return
-    if msg.content == "-sado":
-        for role in msg.author.roles:
-            if role.name == 'chad(s)':
-                await msg.channel.send("sigma > chad")
-                return
-        await msg.channel.send("sado makin dosas")
+    if msg.content == "-sadopy":
+        await msg.add_reaction("<:downvote:931014167559798835>")
+        options = ["Sadopy is making some dosas. Leave him alone","Chad","SIUUUUUUUUU","Sadopy is training",
+        "Sadopy is having tea right now","https://giffiles.alphacoders.com/138/13815.gif",
+        "https://c.tenor.com/YXkNDKQBHTgAAAAd/chad-attacks-nothing-sado-yasutora.gif",
+        "Stop bothering Sadopy",
+        "Go back to work"]
+        await msg.channel.send(random.choice(options))
         return
-    
 
-client.run(config.token)
+@bot.event
+async def on_member_join(user):
+    sends = []
+
+@bot.event
+async def on_member_remove(user):
+    sens=[]
+
+bot.run(config.token)
