7823bd310a13e6fdcf04b144d417efe3ce285ec8
Tejas Kamtam <tejaskamtam@yahoo.com>
2022-01-21 20:09:21 -0600

    unique class implemented

--- bot.py
+++ bot.py
@@ -3,43 +3,43 @@ import discord
 import config
 import random
 
-#   variables
-bot = discord.Client()
-ancmt = bot.get_channel('759670370558541845')
+bot = discord.Client
 
-@bot.event
-async def on_ready():
-    print('sadopy is ready')
+# Custom Bot class
+class Bot(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
-@bot.event
-async def on_message(msg):
-    if msg.author == bot.user:
-        return
-    if msg.content == "-sadopy":
-        await msg.add_reaction("<:downvote:931014167559798835>")
-        options = ["Sadopy is making some dosas. Leave him alone","Chad","SIUUUUUUUUU","Sadopy is training",
-        "Sadopy is having tea right now","https://giffiles.alphacoders.com/138/13815.gif",
-        "https://c.tenor.com/YXkNDKQBHTgAAAAd/chad-attacks-nothing-sado-yasutora.gif",
-        "Stop bothering Sadopy", "Go back to work"]
-        await msg.channel.send(random.choice(options))
-        return
+    async def on_ready(self):
+        print('sadopy is ready')
+    
+    async def on_message(self, msg):
+        if msg.author == bot.user:
+            return
+        if msg.content == "-sadopy":
+            await msg.add_reaction("<:downvote:931014167559798835>")
+            options = ["Sadopy is making some dosas. Leave him alone","Chad","SIUUUUUUUUU","Sadopy is training",
+            "Sadopy is having tea right now","https://giffiles.alphacoders.com/138/13815.gif",
+            "https://c.tenor.com/YXkNDKQBHTgAAAAd/chad-attacks-nothing-sado-yasutora.gif",
+            "Stop bothering Sadopy", "Go back to work"]
+            await msg.channel.send(random.choice(options))
+            return
+    
+    async def on_member_join(self, user):
+        opts = ["{user.mention} has joined among us!","Hey {user.mention}! Welcome to the Unswallowed Committee",
+                "{user.mention} joined the server?! LETS GO!!!","{user.mention} joined the server! Salamat, Thanks!"]
+        ancmt = bot.get_channel(config.ancmtid)
+        msg = ancmt.send(random.choice(opts))
+        await msg
+        await msg.add_reaction('<:peepoUpvote:931009571932209152>')
+    
+    async def on_member_remove(self, user):
+        opts = ["{user.mention} fell down a hole","{user.mention} went to play with some other friends",
+                "All my homies hate {user.mention} for leaving the server","{user.mention} bites the dust. F"]
+        ancmt = bot.get_channel(config.ancmtid)
+        msg = ancmt.send(random.choice(opts))
+        await msg
+        await msg.add_reaction('<:downvote:931014167559798835>')
 
-@bot.event
-async def on_member_join(user):
-    opts = ["{user.mention} has joined among us!","Hey {user.mention}! Welcome to the Unswallowed Committee",
-            "{user.mention} joined the server?! LETS GO!!!","{user.mention} joined the server! Salamat, Thanks!"]
-    msg = ancmt.send(random.choice(opts))
-    await msg
-    await msg.add_reaction('<:peepoUpvote:931009571932209152>')
-
-
-@bot.event
-async def on_member_remove(user):
-    opts = ["{user.mention} fell down a hole","{user.mention} went to play with some other friends",
-            "All my homies hate {user.mention} for leaving the server","{user.mention} bites the dust. F"]
-    msg = ancmt.send(random.choice(opts))
-    await msg
-    await msg.add_reaction('<:downvote:931014167559798835>')
-
-
-bot.run(config.token)
+client = Bot()
+client.run(config.token)
